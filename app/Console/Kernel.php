02bc33eba040ff799ae1ede525b51ec69751818e
Slaven <slaven@mindbeat.app>
2024-04-11 23:53:53 +0200

    Smanjiti runs i dodao overlapping

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('runs:run')->everyFiveSeconds();
+         $schedule->command('runs:run')->everyTenSeconds()->withoutOverlapping();
     }
 
     /**
