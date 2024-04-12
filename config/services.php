6fc4c6aa47ade2b1b0e50831a4d65a5a54273d38
Slaven <slaven@mindbeat.app>
2024-04-12 08:51:29 +0200

    social login for productrion

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'https://localhost:1234/auth/callback/facebook',
+        'redirect' => env('SPA_URL') . '/auth/callback/facebook',
     ],
     //https://developers.facebook.com/apps/738174284744376/dashboard/
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://localhost:1234/auth/callback/google',
+        'redirect' => env('SPA_URL') . '/auth/callback/google',
     ],
     //https://console.cloud.google.com/apis/credentials?rapt=AEjHL4OzP57i3ZblDrVjv5mJX1TDVawDYoa4spf9Tt9658W0NbosK7WDqyw8pspIRzwg5iYK3e-6lAV5vebRspwPC3o8QAhfJ1bpOmHiy4IQZ2sY93IA-TM&project=parent-ai-login
 
