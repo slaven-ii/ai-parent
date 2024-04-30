e06f464b1b245211454858e3fe74ac1f0fce8ad5
Slaven <slaven@mindbeat.app>
2024-04-30 17:32:52 +0200

    email verification cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register', 'forgot-password', 'reset-password'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register', 'forgot-password', 'reset-password', 'email/*'],
 
     'allowed_methods' => ['*'],
 
